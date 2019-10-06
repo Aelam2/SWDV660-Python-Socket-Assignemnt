c04aae163a529669f5817ef0c067836a1fe370fe
James Elam <35540173+Aelam2@users.noreply.github.com>
2019-10-06 16:13:18 -0500

    Added ELK Stack
    Added additional logging to Logstack

--- client.py
+++ client.py
@@ -1,4 +1,5 @@
-import socket                
+import socket    
+import random 
   
 s = socket.socket()          
 port = 9500                
@@ -6,8 +7,11 @@ port = 9500
 # connect to localhost port 9500 
 s.connect(('localhost', port)) 
 
+#The none value is for invoking an error to log to the server
+possibleRequestMessages = ['Hello', 'Test', 'Bye']
+
 #Send data to the server
-req = s.send('Hello')
+req = s.send(random.choice(possibleRequestMessages))
 
 # receive data from the server 
 res = s.recv(1024)
--- server.py
+++ server.py
@@ -1,25 +1,36 @@
 import socket
+import logging
+import logstash
+import sys
+
+host = '54.237.168.58'
+
+test_logger = logging.getLogger('python-application-log')
+test_logger.setLevel(logging.INFO)
+test_logger.addHandler(logstash.LogstashHandler(host, 5959, version=1))
 
 s = socket.socket()
 port = 9500
 s.bind(('', port)) 
-print("Socket bound to port", port)
+test_logger.info("Socket bound to port {}".format(port))
 
 s.listen(5)
-print("Socket is listening")
+test_logger.info("Socket is listening")
 
 while True:
     conn, addr = s.accept()
-    print("Connection from", addr)
-
     data = conn.recv(1024).decode()
-    print('Client data:', data)
-    
+
     message = ''
     if(data.lower() == 'hello'):
         message = 'Hi'
+        test_logger.info('Connected IP Address: {}. Request Data: {}. Response Data: {}'.format(addr, data, message))
+
     else:
         message = 'Goodbye'
+        test_logger.warning('Connected IP Address: {}. Request Data: {}. Response Data: {}'.format(addr, data, message))
+
+
 
     conn.send(message.encode())
     conn.close()
